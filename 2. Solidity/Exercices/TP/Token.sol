a92f4beba2c375b2d771807154d8a2547f0fcd9f
An0rak21 <massive.dynamics021@gmail.com>
2024-02-05 18:25:10 +0100

    Rename:
    Separate token management to an autonomous contract

--- 2. Solidity/Exercices/TP/Token.sol
+++ 2. Solidity/Exercices/TP/Token.sol
@@ -4,9 +4,9 @@ pragma solidity >= 0.8 < 0.9;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
-contract Crowdsale is ERC20 {
-    constructor() ERC20("An0raKToken", "ANO"){
-        
+contract Token is ERC20 {
+    constructor(uint256 _initialSupply) ERC20("An0raKToken", "ANO"){
+        _mint(msg.sender, initialSupply);
     }
 
 }
