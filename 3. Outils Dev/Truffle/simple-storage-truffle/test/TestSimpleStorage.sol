163c1ec69fa6383dc326516b8ef983df076cc6df
An0rak21 <massive.dynamics021@gmail.com>
2024-02-18 22:24:16 +0100

    Add dependencies and test files for SimpleStorage contract

---
+++ 3. Outils Dev/Truffle/simple-storage-truffle/test/TestSimpleStorage.sol
@@ -0,0 +1,11 @@
+const path = require("path");
+module.exports = {
+ contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+ networks: {
+ develop: {
+ host: "localhost",
+ port: 8545,
+ network_id: "*"
+  }
+ }
+};
