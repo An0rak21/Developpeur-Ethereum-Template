d16806988babf4970182806b9cb85af4d50ace30
AnOraK21 <massive.dynamics021@gmail.com>
2024-02-20 11:35:50 +0100

    Add Hardhat testing files and configuration

--- 3. Outils Dev/Truffle/simple-storage-truffle/truffle-config.js
+++ 3. Outils Dev/Truffle/simple-storage-truffle/truffle-config.js
@@ -1,22 +1,14 @@
+const path = require("path");
 module.exports = {
-  networks: {
-    development: {
-     host: "127.0.0.1",
-     port: 8545,
-     network_id: "*",
-    },
-  },
-  
-  // Ajouter cette partie (ou décommenter directement dans le fichier):
-  compilers: {
-    solc: {
-      version: "0.8.19", // Récupérer la version exacte de solc-bin (par défaut : la  version de truffle)
-      settings: {  // Voir les documents de solidity pour des conseils sur l'optimisation et l'evmVersion
-        optimizer: {
-        enabled: false,
-        runs: 200
-        },
-      }
-    },
-  },
- }; 
+ // See
+     
+ // to customize your Truffle configuration!
+ contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+ networks: {
+ develop: {
+ host: "localhost",
+ port: 8545,
+ network_id: "*"
+  }
+ }
+};
